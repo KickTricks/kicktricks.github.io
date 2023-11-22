551b3f3d835fab6441093dbde68bde63e25aeb02
KickTricks <kicktricks@outlook.com>
2023-11-22 08:12:32 +0100

    Added get pinned message on load+dev-tools

--- widgets/get-pinned-message/get-pinned-message.js
+++ widgets/get-pinned-message/get-pinned-message.js
@@ -7,28 +7,17 @@ import { APIEndpoints } from '/helpers/api-endpoints.js'
 const kickChannelName = new URLSearchParams(window.location.search).get('kickchannel')
 const scale = new URLSearchParams(window.location.search).get('scale')
 const pinnedMessageContainer = document.querySelector('.pinned-message')
+let subscriberBadges, pinnedMessagePendingDeletionTimeout
+
 
 if (scale > 0) {
   document.querySelector('html').style.fontSize = `${14 * scale}px`
 }
 
-APIEndpoints.getChannelData({ kickChannelName }).then((data) => {
-  const chatroomId = data.chatroom.id
-  const subscriberBadges = data.subscriber_badges
-  let pinnedMessagePendingDeletionTimeout
-
-  const connection = WSConn.connect()
-  connection.onopen = () => {
-    connection.send(WSConn.connectChatroom({ chatroomId }))
-  }
-  connection.onmessage = (evt) => {
-    const data = JSON.parse(evt.data)
-    if (data.event === Events.PinnedMessageCreatedEvent) {
-      const { duration, message } =
-        ModelFactory.Event.PinnedMessageCreatedEvent(JSON.parse(data.data))
-      const content = Emotes.parse(message.content)
-      pinnedMessageContainer.innerHTML = `
-            <p><span class="badges">${message.sender.identity.badges
+const pinMessage = ({ duration, message }) => {
+  const content = Emotes.parse(message.content)
+  pinnedMessageContainer.innerHTML = `
+        <p><span class="badges">${message.sender.identity.badges
     .map((badge) =>
       Badges.getBadge(subscriberBadges, badge.type, badge.count)
     )
@@ -37,12 +26,31 @@ APIEndpoints.getChannelData({ kickChannelName }).then((data) => {
 }">${
   message.sender.username
 }</span>:</span><span class="message"> ${content}</span></p>
-            `
-      pinnedMessageContainer.classList.remove('hidden')
-      window.clearTimeout(pinnedMessagePendingDeletionTimeout)
-      pinnedMessagePendingDeletionTimeout = window.setTimeout(() => {
-        pinnedMessageContainer.classList.add('hidden')
-      }, duration * 60000)
+        `
+  pinnedMessageContainer.classList.remove('hidden')
+  window.clearTimeout(pinnedMessagePendingDeletionTimeout)
+  pinnedMessagePendingDeletionTimeout = window.setTimeout(() => {
+    pinnedMessageContainer.classList.add('hidden')
+  }, duration * 60000)
+}
+
+APIEndpoints.getChannelData({ kickChannelName }).then((data) => {
+  const chatroomId = data.chatroom.id
+  const channelId = data.chatroom.channel_id
+  subscriberBadges = data.subscriber_badges
+  APIEndpoints.getPinnedMessage({ channelId }).then(data => {
+    if(data) {
+      pinMessage(data)
+    }
+  })
+  const connection = WSConn.connect()
+  connection.onopen = () => {
+    connection.send(WSConn.connectChatroom({ chatroomId }))
+  }
+  connection.onmessage = (evt) => {
+    const data = JSON.parse(evt.data)
+    if (data.event === Events.PinnedMessageCreatedEvent) {
+      pinMessage(ModelFactory.Event.PinnedMessageCreatedEvent(JSON.parse(data.data)))
     } else if (data.event === Events.PinnedMessageDeletedEvent) {
       window.clearTimeout(pinnedMessagePendingDeletionTimeout)
       pinnedMessageContainer.classList.add('hidden')
